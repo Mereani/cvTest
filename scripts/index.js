e444cb08c887701ee221559ff14c033a081d4861
Mereani <baratte.mereani@gmail.com>
2020-12-21 17:26:01 +0100

    lien aide

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,4 @@
 // js en cours 
+// lien aide pour le changement de div
+
+// : https://openclassrooms.com/fr/courses/1567926-un-site-web-dynamique-avec-jquery/1569341-manipuler-le-code-html-avec-jquery
